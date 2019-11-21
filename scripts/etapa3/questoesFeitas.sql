68eeb57d08084d30906a8db60b1e6a001eb069b8
Gabriel Max <gabrielmaxcontato@gmail.com>
2019-11-21 09:56:11 -0300

    Correcao na Questão 2

--- scripts/etapa3/questoesFeitas.sql
+++ scripts/etapa3/questoesFeitas.sql
@@ -4,10 +4,14 @@ DROP VIEW TELEFONES_CLIENTES;
 SELECT f.funcao, f.nome, f.matricula, f.cpf FROM FUNCIONARIO f GROUP BY f.funcao, f.nome, f.matricula, f.cpf ORDER BY f.funcao;
 
 -- Questão 2
-SELECT AVG(p.preco_compra - p.preco_venda) FROM PRODUTO p GROUP BY p.preco_compra, p.preco_venda;
+SELECT AVG(p.preco_venda - p.preco_compra) FROM PRODUTO p GROUP BY p.preco_compra, p.preco_venda;
 
 -- Questão 3
 CREATE VIEW TELEFONES_CLIENTES(nome, telefone) AS SELECT c.nome, t.telefone FROM (CLIENTE c join TELEFONE_CLIENTE t on (c.cpf = t.cpf_cliente)) GROUP BY c.nome, t.telefone;
 SELECT nome, telefone FROM TELEFONES_CLIENTES;
 
+-- Questão Incompleta 4
+SELECT * FROM (PRODUTO p join ITEM i on (p.preco_compra != i.preco_produto));
 
+-- Questão 5
+-- SELECT func.nome WHERE (fu.salario > (SELECT AVG(fun.salario) FROM FUNCIONARIO fun)) FROM FUNCIONARIO func;
