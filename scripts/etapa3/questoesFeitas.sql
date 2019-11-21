77a26d03f51ce6ba5217e4e87737e63794dcde50
Gabriel Max <gabrielmaxcontato@gmail.com>
2019-11-21 11:16:19 -0300

    Questão 6 e 7

--- scripts/etapa3/questoesFeitas.sql
+++ scripts/etapa3/questoesFeitas.sql
@@ -7,7 +7,8 @@ SELECT f.funcao, f.nome, f.matricula, f.cpf FROM FUNCIONARIO f GROUP BY f.funcao
 SELECT AVG(p.preco_venda - p.preco_compra) FROM PRODUTO p GROUP BY p.preco_compra, p.preco_venda;
 
 -- Questão 3
-CREATE VIEW TELEFONES_CLIENTES(nome, telefone) AS SELECT c.nome, t.telefone FROM (CLIENTE c join TELEFONE_CLIENTE t on (c.cpf = t.cpf_cliente)) GROUP BY c.nome, t.telefone ORDER BY c.nome;
+CREATE VIEW TELEFONES_CLIENTES(nome, telefone) AS SELECT c.nome, t.telefone FROM (CLIENTE c join TELEFONE_CLIENTE t on (c.cpf = t.cpf_cliente))
+GROUP BY c.nome, t.telefone ORDER BY c.nome;
 SELECT nome, telefone FROM TELEFONES_CLIENTES;
 
 -- Questão 4
@@ -15,3 +16,10 @@ SELECT i.* FROM (PRODUTO p join ITEM i on (p.preco_compra != i.preco_produto));
 
 -- Questão 5
 SELECT f1.nome FROM FUNCIONARIO f1 WHERE f1.salario > (SELECT AVG(f.salario) FROM FUNCIONARIO f) GROUP BY f1.nome;
+
+-- Questão 6
+SELECT c.nome, c.rua, c.num, c.bairro, c.cidade, c.estado FROM CLIENTE c WHERE c.pontos_crm >= (SELECT MAX(c1.pontos_crm) FROM CLIENTE c1)
+GROUP BY c.nome, c.rua, c.num, c.bairro, c.cidade, c.estado;
+
+-- Questão 7
+SELECT * FROM (CLIENTE c join DEPENDENTE d on (c.cpf = d.cpf));
