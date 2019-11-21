864e543d7ed8a663643423d5b0cdad63d9dbc4e0
Gabriel Max <gabrielmaxcontato@gmail.com>
2019-11-21 09:55:32 -0300

    Novos INSERTS e correcao de UPDATE

--- scripts/etapa3/nossoDLLPADRAO.sql
+++ scripts/etapa3/nossoDLLPADRAO.sql
@@ -346,12 +346,20 @@ INSERT INTO FUNCIONARIO (matricula, cpf, identidade, nome, endereco, salario, fu
   UNION ALL SELECT  400, '680.520.250-00', '4567890', 'Dalton', 'Rua D', 4000, 'Padeiro', NULL, NULL FROM dual
   UNION ALL SELECT  500, '670.510.240-90', '5678901', 'Euclides', 'Rua E', 5000, 'Limpeza', NULL, NULL FROM dual
   UNION ALL SELECT  600, '660.500.230-80', '6789012', 'Fernando', 'Rua F', 6000, 'Seguranca', NULL, NULL FROM dual
+  UNION ALL SELECT  700, '660.490.220-80', '6789012', 'Gabriel', 'Rua G', 7000, 'Seguranca', NULL, NULL FROM dual
+  UNION ALL SELECT  800, '660.480.210-80', '6789012', 'Hericles', 'Rua H', 8000, 'Seguranca', NULL, NULL FROM dual
+  UNION ALL SELECT  900, '660.470.200-80', '6789012', 'Ítalo', 'Rua I', 9000, 'Caixa', NULL, NULL FROM dual
 ;
 
 INSERT INTO FILIAL (codigo_identificacao, nome, endereco, telefone, gerente)
   SELECT            1, 'Bom demais', 'Catolé', '4002-8922', 100 FROM dual
   UNION ALL SELECT  2, 'Bom tambem', 'Malvinas', '0800.88', 200 FROM dual
   UNION ALL SELECT  3, 'Bem melhor', 'São José', '0800.99', 300 FROM dual
+  UNION ALL SELECT  4, 'Melhor Ainda', 'Jeremias', '5246-5544', 400 FROM dual
+  UNION ALL SELECT  5, 'Sempre Bom', 'Bodocongó', '9420-4200', 500 FROM dual
+  UNION ALL SELECT  6, 'Melho que Tem', 'Dinamérica', '4444-4441', 600 FROM dual
+  UNION ALL SELECT  7, 'Ainda Bem', 'Liberdade', '0101-0101', 700 FROM dual
+  UNION ALL SELECT  8, 'Bem Bom', 'Serrotão', '1111-0000', 800 FROM dual
 ;
 
 INSERT INTO CATEGORIA(identificador, nome)
@@ -371,19 +379,73 @@ INSERT INTO MARCA(identificador, nome)
 ;
 
 INSERT INTO CLIENTE(cpf, nome, email, pontos_crm, rua, num, cidade, estado, bairro)
-  SELECT            '123456789', 'Gabriel', 'gabriel@email.com', 100, 'R. Zero', '000', 'Cidade Neutra', 'Estado Invisivel', 'Bairro Secreto' FROM dual
-  UNION ALL SELECT  '234567890', 'Afranio', 'afranio@email.com', 99, 'R. Dois', '002', 'Cidade', 'Vista', 'Santa Mamãe' FROM dual
-  UNION ALL SELECT  '345678901', 'Lucas', 'lucas@email.com', 99, 'R. Três', '003', 'Cidade Bela', 'Vista Bela', 'Santa Mamãe' FROM dual
-  UNION ALL SELECT  '456789012', 'Carol', 'carol@email.com', 101, 'R. Quatro', '004', 'Cidade Linda', 'Vista Cega', 'Santa Aparecida' FROM dual
-  UNION ALL SELECT  '567890123', 'Baptista', 'baptista@email.com', 0, 'R. Dez', '100', 'Cidade Incrivel', 'Vista Torta', 'Santa Desaparecida' FROM dual
+  SELECT            '123.456.789-01', 'Gabriel', 'gabriel@email.com', 100, 'R. Zero', '000', 'Cidade Neutra', 'Estado Invisivel', 'Bairro Secreto' FROM dual
+  UNION ALL SELECT  '234.567.890-12', 'Afranio', 'afranio@email.com', 99, 'R. Dois', '002', 'Cidade', 'Vista', 'Santa Mamãe' FROM dual
+  UNION ALL SELECT  '345.678.901-23', 'Lucas', 'lucas@email.com', 99, 'R. Três', '003', 'Cidade Bela', 'Vista Bela', 'Santa Mamãe' FROM dual
+  UNION ALL SELECT  '456.789.012-34', 'Carol', 'carol@email.com', 101, 'R. Quatro', '004', 'Cidade Linda', 'Vista Cega', 'Santa Aparecida' FROM dual
+  UNION ALL SELECT  '567.890.123-45', 'Baptista', 'baptista@email.com', 0, 'R. Dez', '100', 'Cidade Incrivel', 'Vista Torta', 'Santa Desaparecida' FROM dual
+;
+
+INSERT INTO TELEFONE_CLIENTE(telefone, cpf_cliente)
+  SELECT            '4002-8922', '123.456.789-01' FROM dual
+  UNION ALL SELECT  '8888-9999', '123.456.789-01' FROM dual
+  UNION ALL SELECT  '1111-0000', '234.567.890-12' FROM dual
+  UNION ALL SELECT  '1010-1111', '234.567.890-12' FROM dual
+  UNION ALL SELECT  '4444-2222', '345.678.901-23' FROM dual
+  UNION ALL SELECT  '6555-5666', '456.789.012-34' FROM dual
+  UNION ALL SELECT  '4545-4545', '567.890.123-45' FROM dual
+;
+
+INSERT INTO CAIXA(numero_caixa, codigo_filial)
+  SELECT            90, 1 FROM dual
+  UNION ALL SELECT  91, 2 FROM dual
+  UNION ALL SELECT  92, 3 FROM dual
+  UNION ALL SELECT  93, 4 FROM dual
+  UNION ALL SELECT  94, 5 FROM dual
+  UNION ALL SELECT  95, 6 FROM dual
+  UNION ALL SELECT  96, 7 FROM dual
+  UNION ALL SELECT  97, 8 FROM dual
+;
+
+INSERT INTO ORDEM_COMPRA(numero_nota_fiscal, data_hora, cpf_cliente, codigo_filial, matricula_funcionario, numero_caixa)
+  SELECT            400, timestamp '2017-10-12 21:22:23', '123.456.789-01', 1, 100, 90 FROM dual
+  UNION ALL SELECT  401, timestamp '2018-10-12 21:22:23', '123.456.789-01', 1, 100, 91 FROM dual
+  UNION ALL SELECT  402, timestamp '2019-10-12 21:22:23', '234.567.890-12', 2, 300, 91 FROM dual
+  UNION ALL SELECT  403, timestamp '2020-10-12 21:22:23', '234.567.890-12', 3, 300, 92 FROM dual
+  UNION ALL SELECT  404, timestamp '2021-10-12 21:22:23', '567.890.123-45', 4, 100, 93 FROM dual
+;
+
+INSERT INTO FORNECEDOR(cnpj, nome, endereco, email, id_categoria)
+  SELECT            'cnpj500.111.000-22', 'Distribuidora Ora', 'Rua Gago Ago', 'gagagaguinho@email.com', 1 FROM dual
+  UNION ALL SELECT  'cnpj500.111.000-23', 'Distribuidora De Dora', 'Rua Dora Chika', 'dorinha@email.com', 1 FROM dual
+  UNION ALL SELECT  'cnpj500.111.000-24', 'Distribuidora Dona Chica', 'Rua Chicabom', 'dona.chica@email.com', 2 FROM dual
+  UNION ALL SELECT  'cnpj500.111.000-25', 'Distribuidora Falida', 'Rua Sem Asfalto', 'falindo@email.com', 2 FROM dual
+  UNION ALL SELECT  'cnpj500.111.000-26', 'Distribuidora Bem Demais', 'Rua Eterna', 'bem.demais@email.com', 3 FROM dual
+;
+
+INSERT INTO SOLICITACAO(identificador, data_solicitacao, data_prevista, data_entrega, valor_compra, prazo_pagamento, codigo_filial, cnpj_fornecedor)
+  SELECT            70, TO_DATE('14/10/2019'), TO_DATE('15/10/2019'), TO_DATE('15/10/2019'), 100.00, TO_DATE('14/10/2019'), 1, 'cnpj500.111.000-22' FROM dual
+  UNION ALL SELECT  71, TO_DATE('17/10/2019'), TO_DATE('18/10/2019'), TO_DATE('18/10/2019'), 100.00, TO_DATE('17/10/2019'), 1, 'cnpj500.111.000-22' FROM dual
+  UNION ALL SELECT  72, TO_DATE('20/10/2019'), TO_DATE('21/10/2019'), TO_DATE('21/10/2019'), 100.00, TO_DATE('20/10/2019'), 1, 'cnpj500.111.000-25' FROM dual
+  UNION ALL SELECT  73, TO_DATE('22/10/2019'), TO_DATE('23/10/2019'), TO_DATE('23/10/2019'), 100.00, TO_DATE('22/10/2019'), 1, 'cnpj500.111.000-25' FROM dual
+  UNION ALL SELECT  74, TO_DATE('24/10/2019'), TO_DATE('25/10/2019'), TO_DATE('25/10/2019'), 100.00, TO_DATE('24/10/2019'), 1, 'cnpj500.111.000-26' FROM dual
+  UNION ALL SELECT  75, TO_DATE('27/10/2019'), TO_DATE('28/10/2019'), TO_DATE('28/10/2019'), 100.00, TO_DATE('27/10/2019'), 1, 'cnpj500.111.000-26' FROM dual
+;
+
+INSERT INTO NOTA_FISCAL(numero, cnpj, quantidade, data, valor_por_item, identificador_solicitacao)
+  SELECT            800, 'cnpj100.000.111-22', 10, TO_DATE('15/10/2019'), 100.00, 70 FROM dual
+  UNION ALL SELECT  801, 'cnpj100.000.111-23', 20, TO_DATE('16/10/2019'), 50.00, 71 FROM dual
+  UNION ALL SELECT  802, 'cnpj100.000.111-24', 5, TO_DATE('17/10/2019'), 500.00, 71 FROM dual
+  UNION ALL SELECT  803, 'cnpj100.000.111-25', 10, TO_DATE('18/10/2019'), 200.00, 71 FROM dual
+  UNION ALL SELECT  804, 'cnpj100.000.111-26', 50, TO_DATE('19/10/2019'), 20.00, 71 FROM dual
 ;
 
 INSERT INTO ITEM(identificador, num_nota_fiscal_ordem, numero_nota_fiscal, quantidade, preco_produto, desconto)
-  SELECT             1000, NULL, NULL, 10, 100.00, 0 FROM dual
-  UNION ALL SELECT   2000, NULL, NULL, 9, 2.50, 0 FROM dual
-  UNION ALL SELECT   3000, NULL, NULL, 9, 200.00, 0 FROM dual
-  UNION ALL SELECT   4000, NULL, NULL, 9, 1.00, 0 FROM dual
-  UNION ALL SELECT   5000, NULL, NULL, 9, 999.00, 0 FROM dual
+  SELECT             1000, 400, 800, 10, 100.00, 0 FROM dual
+  UNION ALL SELECT   2000, 401, 801, 9, 2.50, 0 FROM dual
+  UNION ALL SELECT   3000, 402, 802, 9, 200.00, 0 FROM dual
+  UNION ALL SELECT   4000, 403, 803, 9, 1.00, 0 FROM dual
+  UNION ALL SELECT   5000, 404, 804, 9, 999.00, 0 FROM dual
 ;
 
 
@@ -395,22 +457,9 @@ INSERT INTO PRODUTO (codigo_identificacao, nome, descricao, margem_lucro, codigo
   UNION ALL SELECT   14,'Feijao', 'Feijao Preto', 2.00, 2, 200, 3.00, 5.00, TO_DATE('12/09/2019'), TO_DATE('12/02/2020'), 103, 5 FROM dual
 ;
 
-UPDATE FUNCIONARIO SET codigo_filial = 1 WHERE matricula = 100;
-UPDATE FUNCIONARIO SET codigo_filial = 1 WHERE matricula = 200;
-UPDATE FUNCIONARIO SET codigo_filial = 1 WHERE matricula = 300;
-UPDATE FUNCIONARIO SET codigo_filial = 1 WHERE matricula = 400;
-UPDATE FUNCIONARIO SET codigo_filial = 1 WHERE matricula = 500;
-UPDATE FUNCIONARIO SET codigo_filial = 1 WHERE matricula = 600;
-
-
-UPDATE FUNCIONARIO SET matricula_supervisor = 200 WHERE matricula = 600; -- Empacotador supervisiona Segurança
-UPDATE FUNCIONARIO SET matricula_supervisor = 100 WHERE matricula = 200; -- Caixa supervisiona Empacotador
-UPDATE FUNCIONARIO SET matricula_supervisor = 100 WHERE matricula = 100;
-UPDATE FUNCIONARIO SET matricula_supervisor = 200 WHERE matricula = 500; -- Empacotador supervisiona Limpeza
-UPDATE FUNCIONARIO SET matricula_supervisor = 600 WHERE matricula = 400; -- Seguranca supervisiona Padeiro
-UPDATE FUNCIONARIO SET matricula_supervisor = 500 WHERE matricula = 300 -- Limpeza supervisiona Açougueiro
-
-
-
-
-
+UPDATE FUNCIONARIO f SET f.codigo_filial = 1, f.matricula_supervisor = 100 WHERE f.matricula = 100;
+UPDATE FUNCIONARIO g SET g.codigo_filial = 1, g.matricula_supervisor = 100 WHERE g.matricula = 200;
+UPDATE FUNCIONARIO h SET h.codigo_filial = 2, h.matricula_supervisor = 500 WHERE h.matricula = 300; 
+UPDATE FUNCIONARIO i SET i.codigo_filial = 3, i.matricula_supervisor = 600 WHERE i.matricula = 400;
+UPDATE FUNCIONARIO j SET j.codigo_filial = 4, j.matricula_supervisor = 200 WHERE j.matricula = 500;
+UPDATE FUNCIONARIO k SET k.codigo_filial = 5, k.matricula_supervisor = 200 WHERE k.matricula = 600;
