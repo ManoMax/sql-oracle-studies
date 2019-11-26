4fc6df766f9615810a72624fc2811455c4ad907a
Gabriel Max <gabrielmaxcontato@gmail.com>
2019-11-25 22:41:05 -0300

    15/20

--- scripts/etapa3/questoesFeitas.sql
+++ scripts/etapa3/questoesFeitas.sql
@@ -1,7 +1,3 @@
-DROP VIEW TELEFONES_CLIENTES;
-DROP VIEW ACOUGUEIRO_DANTAS;
-DROP VIEW CLIENTES_ESTRANGEIROS;
-
 -- Questão 1
 SELECT f.funcao, f.nome, f.matricula, f.cpf FROM FUNCIONARIO f GROUP BY f.funcao, f.nome, f.matricula, f.cpf ORDER BY f.funcao;
 
@@ -9,10 +5,8 @@ SELECT f.funcao, f.nome, f.matricula, f.cpf FROM FUNCIONARIO f GROUP BY f.funcao
 SELECT AVG(p.preco_venda - p.preco_compra) FROM PRODUTO p GROUP BY p.preco_compra, p.preco_venda;
 
 -- Questão 3
-CREATE VIEW TELEFONES_CLIENTES(nome, telefone) AS SELECT c.nome, t.telefone FROM (CLIENTE c join TELEFONE_CLIENTE t on (c.cpf = t.cpf_cliente))
+CREATE OR REPLACE VIEW TELEFONES_CLIENTES(nome, telefone) AS SELECT c.nome, t.telefone FROM (CLIENTE c join TELEFONE_CLIENTE t on (c.cpf = t.cpf_cliente))
 GROUP BY c.nome, t.telefone ORDER BY c.nome;
--- Testando Questão 3
-SELECT nome, telefone FROM TELEFONES_CLIENTES;
 
 -- Questão 4
 SELECT i.* FROM (PRODUTO p join ITEM i on (p.preco_compra != i.preco_produto));
@@ -45,34 +39,30 @@ SELECT * FROM SOLICITACAO s WHERE s.valor_compra > (SELECT AVG(s1.valor_compra) 
 SELECT AVG(i.quantidade) FROM ITEM i;
 
 -- Questão 13
-CREATE VIEW ACOUGUEIRO_DANTAS(cpf, data_nasc, nome, matricula_funcionario) AS SELECT d.cpf, d.data_nasc, d.nome, d.matricula_funcionario
+CREATE OR REPLACE VIEW ACOUGUEIRO_DANTAS(cpf, data_nasc, nome, matricula_funcionario) AS SELECT d.cpf, d.data_nasc, d.nome, d.matricula_funcionario
 FROM (DEPENDENTE d join (SELECT * FROM FUNCIONARIO f1 WHERE f1.nome = 'Dantas' and f1.funcao = 'Açougueiro') f on (d.matricula_funcionario = f.matricula))
 GROUP BY d.cpf, d.data_nasc, d.nome, d.matricula_funcionario;
--- Testando Questão 13
-SELECT cpf, data_nasc, nome, matricula_funcionario FROM ACOUGUEIRO_DANTAS;
 
 -- Questão 14
-CREATE VIEW CLIENTES_ESTRANGEIROS(cpf, nome, pontos_crm, rua, num, cidade, estado, bairro, telefone) AS SELECT c.cpf, c.nome, c.pontos_crm, c.rua, c.num, c.cidade, c.estado, c.bairro, t.telefone 
+CREATE OR REPLACE VIEW CLIENTES_ESTRANGEIROS(cpf, nome, pontos_crm, rua, num, cidade, estado, bairro, telefone) AS SELECT c.cpf, c.nome, c.pontos_crm, c.rua, c.num, c.cidade, c.estado, c.bairro, t.telefone 
 FROM (CLIENTE c left join TELEFONE_CLIENTE t on (c.cpf = t.cpf_cliente))
 WHERE c.cidade <> 'Campina Grande' and (NOT EXISTS (SELECT * FROM TELEFONE_CLIENTE d WHERE c.cpf = t.cpf_cliente));
 
-SELECT * FROM CLIENTE;
-
-SELECT * FROM TELEFONE_CLIENTE;
-
---Testando Questão 14
-SELECT nome, cidade, telefone FROM CLIENTES_ESTRANGEIROS;
-
+-- Questão 15
+                                                    
+ALTER TABLE FORNECEDOR ADD CONSTRAINT emailCorretoFornecedor
+CHECK (REGEXP_INSTR(email, '^[[:alnum:]_]+@[[:alnum:]]+((\.)[[:alpha:]]+)+$') > 0);
 
 -- QUESTÃO 16
 CREATE OR REPLACE TRIGGER CNPJ_NAO_CADASTRADO 
-BEFORE INSERT ON NOTA_FISCAL 
-FOR EACH ROW 
-DECLARE total_cnpj number;
-BEGIN SELECT COUNT(*) AS total_cnpj FROM FORNECEDOR WHERE cnpj = :NEW.cnpj IF (total_cnpj = 0) 
-THEN raise_application_error(-20502,'CNPJ não cadastrado');
-END IF;
-END CNṔJ_NAO_CADASTRADO;
+  BEFORE INSERT ON NOTA_FISCAL 
+  FOR EACH ROW 
+  DECLARE total_cnpj number;
+    BEGIN
+      SELECT COUNT(*) AS total_cnpj FROM FORNECEDOR WHERE cnpj = :NEW.cnpj IF (total_cnpj = 0) THEN
+        raise_application_error(-20502,'CNPJ não cadastrado');
+      END IF;
+    END CNPJ_NAO_CADASTRADO;
 
 
 
