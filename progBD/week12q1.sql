cd4d1ef05fb202acc2f7ea8cb07e72f13e30e9f2
Gabriel Max <gabrielmaxcontato@gmail.com>
2021-06-03 13:16:38 -0300

    feat: Functions ProgBD - question 1 - week 12

---
+++ progBD/week12q1.sql
@@ -0,0 +1,6 @@
+/*
+Crie um bloco PL/SQL anônimo para recuperar o employee_id e o last_name dos empregados.
+Carregue o resultado recuperado em uma PL/SQL table (o índice da PL/SQL table deve corresponder ao employee_id recuperado).
+A partir da PL/SQL table, imprima na tela o last_name dos empregados.
+Execute o bloco PL/SQL criado. Mostre o código e o resultado da execução. Crie apenas o que está sendo solicitado.
+*/
