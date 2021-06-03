46d9628a9f3ce8e3b548df591f9ddf5152bc2292
Gabriel Max <gabrielmaxcontato@gmail.com>
2021-06-03 13:18:01 -0300

    feat: Functions ProgBD - question 2 - week 12

---
+++ progBD/week12q2.sql
@@ -0,0 +1,8 @@
+/*
+  Altere o código da questão anterior para recuperar não apenas as colunas employee_id e last_name, mas sim todas as colunas de employees.
+Novamente, carregue o resultado recuperado em uma PL/SQL table (o índice da PL/SQL table deve corresponder ao employee_id recuperado).
+A partir da PL/SQL table, imprima na tela o last_name, email e job_id dos empregados.
+Execute o bloco PL/SQL criado.
+Mostre o código e o resultado da execução.
+Crie apenas o que está sendo solicitado.
+*/
